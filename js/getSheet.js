80737215d71eb8a7d22736cb58f2713b9fb51f91
pokoed <20041230andy@gmail.com>
2021-04-26 09:08:30 +0900

    design refresh

--- js/getSheet.js
+++ js/getSheet.js
@@ -1,3 +1,22 @@
+let isFirst = true;
+let isGrade = true;
+let grade;
+
+const setAnimation = (ele) => {
+    // animation
+    ele.classList.add("showData");
+    ele.classList.add("animation-init");
+    setTimeout(function () {
+        ele.classList.add("animation-fade");
+    }, 50);
+}
+const setAnimation2 = (ele) => {
+    // animation
+    setTimeout(function () {
+        ele.classList.add('animation-fade2')
+    }, 50);
+}
+
 let jsonp = function (url) {
     let script = window.document.createElement('script');
     script.async = true;
@@ -19,7 +38,7 @@ let jsonp = function (url) {
     window.document.getElementsByTagName('head')[0].appendChild(script);
 };
 
-let parse = function (data) {
+let parse = (data) => {
     let column_length = data.table.cols.length;
     if (!column_length || !data.table.rows.length) {
         return false;
@@ -69,64 +88,84 @@ let query = function (sql, sheetName, callback) {
     return jsonp(url); // Call JSONP helper function
 }
 
-let my_callback = function (data) {
-    data = parse(data); // Call data parser helper function
+let my_callback = (data) => {
+    if (isGrade) {
+        data = parse(data); // Call data parser helper function
 
-    //AND THEN WHATEVER YOU WANT 
-    for (let i = 0; i < datas.length; i++) {
-        if (JSON.stringify(datas[i]) == JSON.stringify(data)) {
-            return false;
+        //AND THEN WHATEVER YOU WANT 
+        for (let i = 0; i < datas.length; i++) {
+            if (JSON.stringify(datas[i]) == JSON.stringify(data)) {
+                return false;
+            }
         }
-    }
 
-    datas.push(data);
+        datas.push(data);
 
-    //table memo
-    // let table = document.querySelector("#showData table");
-    // if (table === null || table == undefined) {
-    //     // CREATE DYNAMIC TABLE.
-    //     table = document.createElement("table");
+        // CREATE DYNAMIC SELECT.
+        let select = document.createElement("select");
+        select.setAttribute('id', 'gradeSel');
 
-    //     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-    //     let tr = table.insertRow(-1); // TABLE ROW.
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-    //     // ADD JSON DATA TO THE TABLE AS ROWS.
-    //     for (let i = 1; i < data.length; i++) {
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        let opt = document.createElement('option');
+        select.appendChild(opt);
+        for (let i = 1; i < data.length; i++) {
+            let opt = document.createElement('option'); // SELECT OPTION.
+            for (let j = 0; j < 1; j++) {
+                opt.value = data[i][''];
+                opt.text = data[i][''];
+                select.appendChild(opt);
+            }
+        }
 
-    //         tr = table.insertRow(-1);
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        let divContainer = document.querySelector('.container');
+        divContainer.innerHTML = "";
+        let newDiv = document.createElement('div')
+        newDiv.id = 'showGrade'
+        let text = document.createElement('h3');
+        text.innerHTML = '기수';
+        newDiv.appendChild(text);
 
-    //         for (let j = 0; j < 1; j++) {
-    //             let tabCell = tr.insertCell(-1);
-    //             tabCell.innerHTML = data[i][''];
-    //         }
-    //     }
+        newDiv.appendChild(select);
+        setAnimation(newDiv);
+        divContainer.appendChild(newDiv)
+        let showData = document.createElement('div');
+        showData.id = 'showData';
+        divContainer.appendChild(showData);
 
-    //     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-    //     let divContainer = document.getElementById("showData");
-    //     divContainer.innerHTML = "";
-    //     divContainer.appendChild(table);
-    // } else {
+        grade = document.querySelector('#gradeSel')
+        grade.addEventListener('change', () => {
+            let div = document.querySelector('#showData');
+            div.innerHTML = '';
+            datas = [];
+            query(`SELECT * WHERE A != 'null' ORDER BY A ASC`, grade.value, 'my_callback');
+        });
+        isGrade = false;
+    } else {
+        data = parse(data); // Call data parser helper function
 
-    //     // ADD JSON DATA TO THE TABLE AS ROWS.
-    //     for (let i = 0; i < data.length; i++) {
+        //AND THEN WHATEVER YOU WANT 
+        for (let i = 0; i < datas.length; i++) {
+            if (JSON.stringify(datas[i]) == JSON.stringify(data)) {
+                return false;
+            }
+        }
 
-    //         let tr = table.insertRow();
+        datas.push(data);
 
-    //         for (let j = 0; j < 1; j++) {
-    //             let tabCell = tr.insertCell(-1);
-    //             tabCell.innerHTML = data[i][''];
-    //         }
-    //     }
-    // }
-    let select = document.querySelector("#showData select");
+        let select = document.querySelector("#showData");
 
         // CREATE DYNAMIC SELECT.
         select = document.createElement("select");
         select.setAttribute('id', 'selName');
 
         // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-        
+
         // ADD JSON DATA TO THE TABLE AS ROWS.
+        let opt = document.createElement('option');
+        select.appendChild(opt);
         for (let i = 1; i < data.length; i++) {
             let opt = document.createElement('option'); // SELECT OPTION.
             for (let j = 0; j < 1; j++) {
@@ -139,15 +178,69 @@ let my_callback = function (data) {
         // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
         let divContainer = document.getElementById("showData");
         divContainer.innerHTML = "";
+
+        let text = document.createElement('h3');
+        text.innerHTML = '이름';
+        divContainer.appendChild(text);
+
         divContainer.appendChild(select);
+        let lastDiv = document.getElementById('showGrade')
+        setAnimation2(lastDiv);
+        setAnimation(divContainer);
+        let name = document.getElementById('selName')
+        name.addEventListener('change', () => {
+            if (isFirst) {
+                let btn = document.createElement('button');
+                btn.innerText = '출석';
+                btn.id = 'sendBtn';
+                document.querySelector('.container').appendChild(btn);
+                let lastDiv = document.getElementById('showData')
+                setAnimation2(lastDiv);
+                setAnimation(btn);
+                isFirst = false;
+            }
+            const url = 'https://script.google.com/macros/s/AKfycbz2fhg1c1OGz-0eX-8Tjwz0likHmyUT8xI8wYRuIptctS4Ej6ZT/exec';
+            const send = document.querySelector('#sendBtn');
+            const today = new Date();
+
+            send.addEventListener('click', () => {
+                const name = document.querySelector('#selName').value;
+                const grade = document.querySelector('#gradeSel').value;
+
+                let data = {
+                    '기수': grade,
+                    '이름': name,
+                    '일자': today.toLocaleDateString(),
+                    '시간': today.toLocaleTimeString(),
+                }
+                $.ajax({
+                    method: "POST",
+                    url: url,
+                    data: data,
+                    success: function (response) {
+                        alert('출석 완료.');
+                    }
+                });
+
+                // fetch(url, {
+                //     method: 'POST', 
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: data,
+                // }).then((res) => alert('출석 완료.'));
+            });
+        });
+    }
 }
 
 let datas = [];
 
-let grade = document.querySelector('#gradeSel');
-grade.addEventListener('change', () => {
-    let div = document.querySelector('#showData');
+window.onload = () => {
+    let div = document.querySelector('.container');
     div.innerHTML = '';
     datas = [];
-    query(`SELECT * WHERE A != 'null' ORDER BY A ASC`, grade.value, 'my_callback');
-});
+    query(`SELECT * WHERE A != 'null' ORDER BY A DESC`, '기수목록', 'my_callback');
+}
+
+
